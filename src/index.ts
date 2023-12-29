a7822a020603188e298bda9a16b17dff4ebd8587
StefMa <StefMaDev@outlook.com>
2023-12-29 14:34:05 +0100

    Testing

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,16 @@ export async function build(options: BuildOptions) {
     console.log(options.workPath)
     console.log(options.repoRootPath)
     console.log(__dirname)
-    const x = await glob("**", join(__dirname, "../handler"))
-    const handlerPath = new FileFsRef({ fsPath: join(__dirname, "../handler") })
-    console.log(handlerPath.fsPath)
-    const startHandlerPath = new FileFsRef({ fsPath: join(__dirname, "../startHandler.sh") })
-    startHandlerPath.mode = 33261 // 0755;
-    console.log(startHandlerPath.fsPath)
+    const handlerFiles = await glob("**", join(__dirname, "../handler"))
+    const startHandlerFile = new FileFsRef({ fsPath: join(__dirname, "../startHandler.sh") })
+    startHandlerFile.mode = 33261 // 0755;
+    console.log(startHandlerFile.fsPath)
     const lambda = new Lambda({
         files: {
-            //'handler': handlerPath,
-            ...x,
-            startHandlerPath
+            ...handlerFiles,
+            startHandlerFile
         },
-        handler: startHandlerPath.fsPath,
+        handler: startHandlerFile.fsPath,
         runtime: "provided.al2023"
     })
 
--- startHandler.sh
+++ startHandler.sh
@@ -1,2 +1,1 @@
-cd handler
 ./gradlew run
