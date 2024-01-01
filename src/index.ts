ec4f251ecb4356fc9efc3868e52a3a197383f28f
StefMa <StefMaDev@outlook.com>
2024-01-01 11:19:03 +0100

    Testing

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export async function build(options: BuildOptions) {
     console.log(options.repoRootPath)
     console.log(__dirname)
     const handlerFiles = await glob("**", join(__dirname, "../handler"))
+    const java = await glob("**", join(__dirname, "../amazon-corretto-11.0.21.9.1-linux-x86"))
     const bootstrap = new FileFsRef({ fsPath: join(__dirname, "../bootstrap") })
     bootstrap.mode = 33261 // 0755;
     const startHandlerFile = new FileFsRef({ fsPath: join(__dirname, "../startHandler.sh") })
@@ -28,6 +29,7 @@ export async function build(options: BuildOptions) {
     //await execCommand(u)
     const lambda = new Lambda({
         files: {
+            ...java,
             ...handlerFiles,
             startHandlerFile,
             bootstrap
